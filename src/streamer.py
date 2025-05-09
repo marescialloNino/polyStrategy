75c8b2af63c172a763bac247923049fb24b02245
RichardNixon <nicola.schiavo@studenti.unipd.it>
2025-05-09 21:30:56 +0200

    update imports

--- src/streamer.py
+++ src/streamer.py
@@ -1,14 +1,15 @@
 import asyncio
-from src.data_streamer import MarketDataStreamer
+from data_streamer.data_streamer import MarketDataStreamer
 
 async def main():
     
     streamer = MarketDataStreamer(
-        slug="ETH-DAI",            # folder name to store the CSV
-        token1="",                 # replace token IDs
-        token2="0",
-        interval_seconds=60        # how often to pull data
+        slug="Cavaliers_Pacers",            # folder name to store the CSV
+        token1="75257668319315408269469801686878365015769536442828459002998616661971033609731",                 # replace token IDs
+        token2="106744726778569417882808847688355733040025048364560998914989057253648296994787",
+        interval_seconds=10        # how often to pull data
     )
+
 
     # This will loop forever until  Ctrl+C
     await streamer.stream()
